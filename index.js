d68b33ccfcdc11f4d99b99bbcaddbec578cf391d
Anna Kioko <annakioko00@gmail.com>
2024-01-14 18:54:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Anna",
+    streetAddress: "Syokimau"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+ 
+  console.log(employee);
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "position", "Manager");
+  console.log(updatedEmployee); 
+  
+  const destructiveUpdate = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Anna");
+  console.log(destructiveUpdate); 
+  
+  const employeeWithoutAddress = deleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(employeeWithoutAddress); 
+  
+  const destructiveDeletion = destructivelyDeleteFromEmployeeByKey(employee, "name");
+  console.log(destructiveDeletion); 
+  
+  console.log(employee); 
+  
